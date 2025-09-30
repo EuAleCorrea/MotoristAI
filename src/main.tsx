3d845c4d8125999fa1bdf52434c16572502f4e91
[dyad] <eu.alecorrea@gmail.com>
2025-09-30 13:38:15 -0300

    [dyad] Corrigindo a alternância de tema - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { ThemeProvider } from "@/components/ThemeProvider"; // Importando o ThemeProvider
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> {/* Adicionando ThemeProvider */}
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme" attribute="class"> {/* Adicionando attribute="class" explicitamente */}
     <AuthProvider>
       <App />
     </AuthProvider>
